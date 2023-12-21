f50bed36af41f7700b2a626dbccbbc8d0bf44279
maryem <maryemlabihi9@gmail.com>
2023-12-21 11:59:03 +0100

    hello

---
+++ resources/views/project.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex saepe ullam est voluptates, nostrum labore aperiam at, deserunt sequi, soluta eveniet ipsam. Quos molestias in neque libero dolores, aperiam ut!</p>
+</body>
+</html>
