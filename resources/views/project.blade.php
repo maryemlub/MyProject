895a190e084903a31f4d139da9a425f44b2a0e48
maryemlub <154520535+maryemlub@users.noreply.github.com>
2023-12-21 14:20:31 +0100

    Update project.blade.php
    modifié

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -7,7 +7,29 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>git</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex saepe ullam est voluptates, nostrum labore aperiam at, deserunt sequi, soluta eveniet ipsam. Quos molestias in neque libero dolores, aperiam ut!</p>
+   <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+    
+    </head>
+    <body class="antialiased">
+    <div class="container mt-5">
+    <h1>Welcome to our website!</h1>
+</div>
+
+    </body>
+</html>
+</body>
+</html>
 </body>
 </html>
