d01d8520f8991300a726061ef256b02807dfc61f
maryemlub <154520535+maryemlub@users.noreply.github.com>
2023-12-21 23:01:37 +0100

    Update project.blade.php
    final modification

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,13 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-   <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,6 +20,4 @@
 </div>
 
     </body>
-</html>
-</body>
 </html>
